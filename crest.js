cc286458659a4a51422852760e3dfd65d9c4d99e
Robert Pitt <rpitt@centiq.co.uk>
2015-02-03 11:51:09 +0000

    Added copyright infomration.

--- crest.js
+++ crest.js
@@ -1,8 +1,13 @@
 /**
  *
- * 
+ * @name Crest
+ * @description Rest library designed to easily configure and access 
+ *              external resources within a Meteor Application.
  *
- * 
+ * @author <rpitt@centiq.co.uk> Robert Pitt
+ * @package centiq:crest
+ * @copyright 2015 Centiq Ltd.
+ * @license http://opensource.org/licenses/MIT
  */
 
 /**
