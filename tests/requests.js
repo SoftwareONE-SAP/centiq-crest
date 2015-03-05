3e79c67eb9a805b0d2df2310fbe274125a3891e3
Peter Choo <pchoo@centiq.co.uk>
2015-03-05 14:34:39 +0000

    Format according to style guides

--- tests/requests.js
+++ tests/requests.js
@@ -1,6 +1,6 @@
 /**
  * @name Crest
- * @description Rest library designed to easily configure and access 
+ * @description Rest library designed to easily configure and access
  *              external resources within a Meteor Application.
  *
  * @author <rpitt@centiq.co.uk> Robert Pitt
@@ -12,85 +12,86 @@
 /**
  * API Tests
  */
-Tinytest.addAsync("Crest API - Requests", function(test, next){
-	/**
-	 * Create a new Crest Resource
-	 */
-	var crest = new Crest({
-		base_url: "http://localhost:3002/"
-	});
+Tinytest.addAsync("Crest API - Requests", function(test, next) {
+  /**
+   * Create a new Crest Resource
+   */
+  var crest = new Crest({
+    base_url: "http://localhost:3002/"
+  });
 
-	/**
-	 * Add a resources
-	 */
-	crest.addResource("posts");
+  /**
+   * Add a resources
+   */
+  crest.addResource("posts");
 
-	/**
-	 * Request Handler
-	 */
-	var _requestHandler = function(response) {
-		test.ok(response);
-		test.equal(response.statusCode, 200);
-		test.equal(typeof response.headers, typeof []);
-		test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
-		test.equal(typeof response.data, typeof []);
-		test.equal(response.data.length, 100);
-	};
+  /**
+   * Request Handler
+   */
+  var _requestHandler = function(response) {
+    test.ok(response);
+    test.equal(response.statusCode, 200);
+    test.equal(typeof response.headers, typeof []);
+    test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
+    test.equal(typeof response.data, typeof []);
+    test.equal(response.data.length, 100);
+  };
 
-	if(Meteor.isServer){
-		_requestHandler(crest.posts.get());
-		return next();
-	}
+  if (Meteor.isServer) {
+    _requestHandler(crest.posts.get());
+    return next();
+  }
 
-	if(Meteor.isClient){
-		crest.posts.get(function(err, response){
-			if(err)
-				throw err;
-			_requestHandler(response);
-			next();
-		});
-	}
+  if (Meteor.isClient) {
+    crest.posts.get(function(err, response) {
+      if (err)
+        throw err;
+      _requestHandler(response);
+      next();
+    });
+  }
 });
 
 /**
  * Test a single root level request
  */
-Tinytest.addAsync("Crest API - Requests - Single Resource", function(test, next){
-	/**
-	 * Create a new Crest Resource
-	 */
-	var crest = new Crest({
-		base_url: "http://localhost:3002/"
-	});
+Tinytest.addAsync("Crest API - Requests - Single Resource", function(test, next) {
+  /**
+   * Create a new Crest Resource
+   */
+  var crest = new Crest({
+    base_url: "http://localhost:3002/"
+  });
 
-	/**
-	 * Add a resources
-	 */
-	crest.addResource("posts");
+  /**
+   * Add a resources
+   */
+  crest.addResource("posts");
 
-	/**
-	 * Request Handler
-	 */
-	var _requestHandler = function(response) {
-		test.ok(response);
-		test.equal(response.statusCode, 200);
-		test.equal(typeof response.headers, typeof []);
-		test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
-		test.equal(typeof response.data, typeof {});
-	};
+  /**
+   * Request Handler
+   */
+  var _requestHandler = function(response) {
+    test.ok(response);
+    test.equal(response.statusCode, 200);
+    test.equal(typeof response.headers, typeof []);
+    test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
+    test.equal(typeof response.data, typeof {});
+  };
 
-	if(Meteor.isServer){
-		_requestHandler(crest.posts.get(1));
-		return next();
-	}
+  if (Meteor.isServer) {
+    _requestHandler(crest.posts.get(1));
+    return next();
+  }
 
-	if(Meteor.isClient)
-		crest.posts.get(1, function(err, response){
-			if(err)
-				throw err;
-			_requestHandler(response);
-			next();
-		});
+  if (Meteor.isClient) {
+    crest.posts.get(1, function(err, response) {
+      if (err)
+        throw err;
+      _requestHandler(response);
+      next();
+    });
+  }
 });
 
 
@@ -98,44 +99,45 @@ Tinytest.addAsync("Crest API - Requests - Single Resource", function(test, next)
 /**
  * Test a single root level request
  */
-Tinytest.addAsync("Crest API - Requests - Child Resource", function(test, next){
-	/**
-	 * Create a new Crest Resource
-	 */
-	var crest = new Crest({
-		base_url: "http://localhost:3002/"
-	});
-	crest.param('X-Test-Token', 'testing');
-	crest.header('X-Test-Token', 'testing');
-	crest.header('Access-Control-Allow-Headers', 'X-Test-Token');
+Tinytest.addAsync("Crest API - Requests - Child Resource", function(test, next) {
+  /**
+   * Create a new Crest Resource
+   */
+  var crest = new Crest({
+    base_url: "http://localhost:3002/"
+  });
+  crest.param('X-Test-Token', 'testing');
+  crest.header('X-Test-Token', 'testing');
+  crest.header('Access-Control-Allow-Headers', 'X-Test-Token');
 
-	/**
-	 * Add a resources
-	 */
-	crest.addResource("posts");
+  /**
+   * Add a resources
+   */
+  crest.addResource("posts");
 
-	/**
-	 * Request Handler
-	 */
-	var _requestHandler = function(response) {
-		test.ok(response);
-		test.equal(response.statusCode, 200);
-		test.equal(typeof response.headers, typeof []);
-		test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
-		test.equal(typeof response.data, typeof {});
-		console.log(response);
-	};
+  /**
+   * Request Handler
+   */
+  var _requestHandler = function(response) {
+    test.ok(response);
+    test.equal(response.statusCode, 200);
+    test.equal(typeof response.headers, typeof []);
+    test.equal(response.headers['content-type'], 'application/json; charset=utf-8');
+    test.equal(typeof response.data, typeof {});
+    console.log(response);
+  };
 
-	if(Meteor.isServer){
-		_requestHandler(crest.posts.get(1));
-		return next();
-	}
+  if (Meteor.isServer) {
+    _requestHandler(crest.posts.get(1));
+    return next();
+  }
 
-	if(Meteor.isClient)
-		crest.posts.get(1, function(err, response){
-			if(err)
-				throw err;
-			_requestHandler(response);
-			next();
-		});
+  if (Meteor.isClient) {
+    crest.posts.get(1, function(err, response) {
+      if (err)
+        throw err;
+      _requestHandler(response);
+      next();
+    });
+  }
 });
